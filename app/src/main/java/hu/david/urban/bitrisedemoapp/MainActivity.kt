a768b4d015ad72d0798b0aee8c0e0e93eacd6cca
Dávid Urbán <david.urban@icellmobilsoft.hu>
2021-05-03 13:49:14 +0200

    Initial commit

---
+++ app/src/main/java/hu/david/urban/bitrisedemoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package hu.david.urban.bitrisedemoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
